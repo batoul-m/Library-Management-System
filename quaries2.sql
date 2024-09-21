d4eb17cf6949ebda04943943d7f5718b1947614e
batoul-m <batoul.2003.m@gmail.com>
2024-09-21 18:09:26 +0300

    Active Borrowers with CTEs

---
+++ quaries2.sql
@@ -0,0 +1,11 @@
+WITH ActiveBorrwers AS(
+    SELECT BorrowerID,Count(*) AS BorrowedBook
+    FROM Loans
+    WHERE DateReturned IS NOT NULL
+    GROUP BY BorrowerID
+    HAVING COUNT(*) >= 2
+
+)
+SELECT AB.BorrowerID , B.FirstName , B.LastName
+FROM ActiveBorrwers AB
+JOIN Borrowers B ON B.BorrowerID = AB.BorrowerID;
